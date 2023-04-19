9388fff221680e2e80748ea10491510242f9622f
Cinthia Cavalheiro <91417907+Cinthiacs@users.noreply.github.com>
2023-04-18 21:58:14 -0300

    Update Valores.java

--- src/Valores/Valores.java
+++ src/Valores/Valores.java
@@ -3,11 +3,6 @@ import java.util.Scanner;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-/*Escreva um algoritmo para ler 2 valores e se o segundo valor informado for ZERO, 
-deve ser lido um novo valor, ou seja, para o segundo valor não pode ser aceito o valor zero 
-e imprimir o resultado da divisão do primeiro valor lido pelo segundo valor lido. 
-(utilizar a estrutura REPITA).*/
-
 public class Valores {
     public static void main(String[] args) throws Exception {
 
